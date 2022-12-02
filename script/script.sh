58a33ac4c1afd618b7f2c822ad95bad7c790aa72
Nicolas <silvanicolas93@gmail.com>
2022-12-02 19:00:20 -0300

    agrego jq y modifico >> para el historico.txt
    Signed-off-by: Nicolas <silvanicolas93@gmail.com>

--- script/script.sh
+++ script/script.sh
@@ -14,7 +14,7 @@ do
 		COMPRA=$(curl 127.0.0.1:5000/usd 2>/dev/null | ./jq .compra)
 		VENTA=$(curl 127.0.0.1:5000/usd 2>/dev/null | ./jq .venta)
 		FECHA=$(date +%F_%H:%M)
-		echo "$FECHA, $COMPRA, $VENTA" > historico.txt
+		echo "$FECHA, $COMPRA, $VENTA" >> historico.txt
 		SYNC_OLD=$SYNC_NEW
 	fi
 done
