b5e0f7409730cf25fb386d2220dbf01d36f3463f
Nicolas <silvanicolas93@gmail.com>
2022-12-02 18:44:34 -0300

    agrego historico.txt y lo vinculo al script.sh
    Signed-off-by: Nicolas <silvanicolas93@gmail.com>

--- script/script.sh
+++ script/script.sh
@@ -2,7 +2,7 @@
 
 SYNC="sync/sync.txt"
 SYNC_OLD=$(stat -c %y $SYNC)
-
+ 
 while (true)
 do
 	sleep 1
@@ -14,7 +14,7 @@ do
 		COMPRA=$(curl 127.0.0.1:5000/usd 2>/dev/null | ./jq .compra)
 		VENTA=$(curl 127.0.0.1:5000/usd 2>/dev/null | ./jq .venta)
 		FECHA=$(date +%F_%H:%M)
-		echo "$FECHA, $COMPRA, $VENTA"
+		echo "$FECHA, $COMPRA, $VENTA" > historico.txt
 		SYNC_OLD=$SYNC_NEW
 	fi
 done
