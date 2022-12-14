852db9a330ce2a41b370dd2a01a6e430e6e9bac5
P3L4 <silvanicolas93@gmail.com>
2022-12-13 22:35:23 -0300

    Subo bootstrap sin funcion
    Signed-off-by: P3L4 <silvanicolas93@gmail.com>

---
+++ bootstrap.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+SYNCPATH=$(pwd)/script/sync/sync.txt
+
+sed "s|\#REPLACEME|$SYNCPATH|" service/service_tpedp.template > service/service_tpedp.service
+
+cp service/service_tpedp.service /etc/systemd/system
+cp service/service_tpedp.timer /etc/systemd/system
+
+systemctl daemon-reload
+systemctl start service_tpedp.timer
+
+docker-compose up -d --build
